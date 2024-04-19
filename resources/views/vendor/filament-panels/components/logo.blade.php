cd1164aa2621d49024e62a4f85a08fb34e5e364d
guishex2001 <91160700+guishex2001@users.noreply.github.com>
2024-04-19 17:43:27 -0300

    Update logo.blade.php
    http a https

--- resources/views/vendor/filament-panels/components/logo.blade.php
+++ resources/views/vendor/filament-panels/components/logo.blade.php
@@ -1,5 +1,5 @@
 <img 
-src="http://codecat.site/nakamas/wp-content/uploads/2024/04/logo_guishex-removebg-preview.png" 
+src="https://codecat.site/nakamas/wp-content/uploads/2024/04/logo_guishex-removebg-preview.png" 
 alt="logo"
 width="60" 
 height="60" 
