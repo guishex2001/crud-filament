d882d8fe72dc9b9ae5b613514675a1c2d21c7da6
guishex2001 <91160700+guishex2001@users.noreply.github.com>
2024-04-22 16:24:04 -0300

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,9 +25,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if ($this->app->environment ('production')) {
-            URL:: forceScheme ('https');
-        };
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
+        
         Schema::defaultStringLength(191);
     }
 }
