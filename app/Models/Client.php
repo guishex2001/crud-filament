073b3bc0c1d7993278ccf95676340417b3481b5b
guishex2001 <91160700+guishex2001@users.noreply.github.com>
2024-04-23 18:38:26 -0300

    Update Client.php

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -6,10 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Client extends Model
 {
-    protected $fillable = ['nombre', 'fecha_pago', 'fecha_vencimiento', 'estado_pago'];
+    protected $fillable = ['nombre', 'fecha_pago', 'fecha_vencimiento']; // Se elimina 'estado_pago' de los fillable
 
     public function tvboxes()
     {
         return $this->hasMany(Tvbox::class,'id');
+    }
+
+    // Se agrega el accesor para el estado de pago
+    public function getEstadoPagoAttribute()
+    {
+        return $this->fecha_vencimiento >= now();
     }
 }
